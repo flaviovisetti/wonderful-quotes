8a36ce3638aabdbb1eb0f1f8c8c1c613b3176b1e
Flavio Visetti Soares <flavio.visetti@gmail.com>
2018-09-14 15:45:43 -0300

    update repository with the exercise code

---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="row">
+    <app-quote v-for="(quote, index) in quotes"
+               @click.native="deleteQuote(index)">
+      <p>{{ quote }}</p>
+    </app-quote>
+  </div>
+</template>
+
+<script>
+import Quote from './Quote.vue'
+
+export default {
+  props: {
+    quotes: Array
+  },
+  methods: {
+    deleteQuote(index) {
+      this.$emit('deleteQuote', index)
+    }
+  },
+  components: {
+    'app-quote': Quote
+  }
+}
+</script>
+
+<style>
+</style>
